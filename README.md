af22f221379283c6c8bc7ec7b7059b38f9daf5ff
nguyen <nguyen.hv172728@gmail.com>
2021-04-25 09:12:16 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# AirSENSE-version-3
+Third version of AirSENSE project
+
+#REQUIREMENTS
+1. Install Visual studio code
+2. Install Platform IO extension
+3. Clone this repo
+4. Add this repo to Platform IO (Add existing project option)
+5. Compile
---
+++ include/secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "HuyenDienChau"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "122345678"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID 1369132		// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "IXZ0UQNVCVV6BG7Z"   // replace XYZ with your channel write API Key
